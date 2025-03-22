df5b15313fc15c403c93c72f702c3ceab869c9b6
aaransmith11 <aaransmith0817@gmail.com>
2025-03-22 18:56:17 +0000

    to do list complete

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState("");
+	const [toDoList, setToDoList] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const deleteToDo = (i) => {
+		let newToDo = toDoList.filter((toDo, index)=>{
+			if (index != i){
+				return toDo
+			}
+		})
+		setToDoList (newToDo)
+	}
+	return (
+		<div className="container">
+			<h1>To-Do List</h1>
+			<ul>
+			<li>
+				<input 
+				type="text" 
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyPress={(e) => {
+					if (e.key === "Enter") {
+						setToDoList(toDoList.concat(inputValue));
+						setInputValue("");
+					}	
+				}}
+				placeholder="List anything else you need to do"></input>
+			</li>
+			{toDoList?.map((t,index) => (
+				<li key={index}>
+					{t} <span onClick={()=>deleteToDo(index)}>X</span>
+				</li>
+		))}
+				
+			</ul>
+			<div>{toDoList.length} tasks</div>
 		</div>
 	);
 };
